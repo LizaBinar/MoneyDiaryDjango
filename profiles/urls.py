0ac012bc155774292ce2dc6b395e2655a7409bd6
LizaBinar <106739245+LizaBinar@users.noreply.github.com>
2022-10-09 17:14:14 +0700

    change architecture in urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import *
+from . import views
 
-urlpatterns = [path('user_profile/<int:pk>/', ShowProfilePageView.as_view(), name='user_profile'),
-               path('create_profile_page/', CreateProfilePageView.as_view(), name='create_user_profile'),
-               path('update_profile_page/<int:pk>/', ProfileUpdateView.as_view(), name='update_user_profile')
+urlpatterns = [path('user_profile/<int:pk>/', views.ShowProfilePageView.as_view(), name='user_profile'),
+               path('create_profile_page/', views.CreateProfilePageView.as_view(), name='create_user_profile'),
+               path('update_profile_page/<int:pk>/', views.ProfileUpdateView.as_view(), name='update_user_profile')
                ]
